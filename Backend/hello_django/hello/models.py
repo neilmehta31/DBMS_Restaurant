7cfeda3d6f8567302d87c0f43b0a3a3a896d6696
Mahavir Chaudhari <mveer.rc@gmail.com>
2021-04-06 02:07:27 +0530

    incomplete model 1

--- Backend/hello_django/hello/models.py
+++ Backend/hello_django/hello/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Customers(models.Model):     #learners tip: first Letter stays capital in the name.
+    Customer_id = models.IntegerField(primary_key = True)
+    Customer_name = models.CharField(max_length = 20)
+    house no. = models.CharField(max_length = 20)
+    street_name = models.CharField(max_length = 20)
+    city = models.CharField(max_length = 20)
+    pincode = models.CharField(max_length = 6)
+    ...
+    #
