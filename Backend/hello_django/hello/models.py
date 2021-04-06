6530f70851073cb45b4a51a6f0503a3f3f3d4d89
Mahavir Chaudhari <mveer.rc@gmail.com>
2021-04-07 01:38:45 +0530

    minor(migrations)

--- Backend/hello_django/hello/models.py
+++ Backend/hello_django/hello/models.py
@@ -8,5 +8,7 @@ class Customers(models.Model):     #learners tip: first Letter stays capital in 
     street_name = models.CharField(max_length = 20)
     city = models.CharField(max_length = 20)
     pincode = models.CharField(max_length = 6)
-    ...
-    #
+    phone_no = models.IntegerField(max_length=10)
+    billing_amt = models.IntegerField()
+    people_accoma = models.IntegerField(max_length = 10)    #tbd
+    # bench_num = models.Foreign_key()                      leave foreign_key for now.
