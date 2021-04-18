37ac2b2ce41e0636d6fcd8c358cd711ecbee3b3a
Mahavir Chaudhari <mveer.rc@gmail.com>
2021-04-18 18:20:40 +0530

    minor

--- Backend/restflask/app.py
+++ Backend/restflask/app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+mysql = MySQL(app)
+
+class flask_mysqldb.MySQL(app)
+    def connection():
+        
 
 @app.route('/')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT user, host FROM mysql.user''')
+    rv = cur.fetchall()
+    return str(rv)
 
-def index():
-    return "hello world"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
