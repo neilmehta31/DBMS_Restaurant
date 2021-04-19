97532f32720fccbb2d97e9d1a0651cf7d19d7ffb
Mahavir Chaudhari <mveer.rc@gmail.com>
2021-04-19 18:07:27 +0530

    minor

--- Backend/restflask/app.py
+++ Backend/restflask/app.py
@@ -15,9 +15,11 @@ def csignup():
     print("knalkd")
     cursor = db.cursor()
     data = request.get_json()
+    
     sql = "SELECT COUNT(*) FROM CUSTOMERS"
     cursor.execute(sql)
     usercount = cursor.fetchall()
+    
     id = int(usercount[0][0])+1
     CUSTOMER_NAME = data['CUSTOMER_NAME']
     PASSWORD = data['PASSWORD']
@@ -28,12 +30,18 @@ def csignup():
     PEOPLE_ACCOMPANYING = data['PEOPLE_ACCOMPANYING']
     TIMESTAMP = data['TIMESTAMP']
     BENCH_NUM = data['BENCH_NUM']
+    
     sql = "INSERT INTO CUSTOMERS VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s )"
     params = [id, CUSTOMER_NAME, EMAIL, PASSWORD, ADDRESS, PHONE_NO, BILLING_AMT, PEOPLE_ACCOMPANYING, TIMESTAMP, BENCH_NUM ]
     cursor.execute(sql,  params)
+    
+    #assign bench to customer.
+
+
     db.commit()
     cursor.close()
-    return "Success"
+
+    return jsonify(True)
 
 @app.route('/login/customer', methods=['POST'])
 def clogin():
@@ -50,14 +58,29 @@ def clogin():
         return jsonify(True)
     else:
         return "email/password does not match, please re-enter"
-@app.route('/tables',methods=['GET'])
-def gettabledetails():
+
+
+@app.route('/bench',methods=['GET'])
+def getbenchdetails():
     cursor = db.cursor()
     sql = "SELECT * FROM BENCH"
     cursor.execute(sql)
     tabledetails = cursor.fetchall()
     return jsonify(tabledetails)
-    
+
+
+# @app.route('/assignbench',methods=['GET', 'POST'])
+# def assignbench():
+#     cursor = db.cursor()
+#     data = request.get_json()
+#     num = data["BENCH_NUM"]
+
+#     # sql = "SELECT * FROM BENCH"
+#     # cursor.execute(sql)
+#     # tabledetails = cursor.fetchall()
+#     return jsonify(True)
+
+
 @app.route('/addtables',methods=['POST'])
 def addtabledetails():
     cursor=db.cursor()
@@ -68,11 +91,10 @@ def addtabledetails():
     cursor.close()
     return jsonify(True)
 
-
 @app.route('/menu',methods=['GET'])
 def getmenu():
     cursor=db.cursor()
-    sql = "SELECT * FROM MEAL;"
+    sql = "SELECT * FROM MEAL;" #"where quantity > 0"
     cursor.execute(sql)
     menu = cursor.fetchall()
     return jsonify(menu)
@@ -84,3 +106,29 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+# customer BILLING_AMT
+#     BENCH_NUM
+#         mealID - prize, 1000
+#         delete the rows from bench meal where BENCH_NUM = CUSTOMERS.BENCH_NUM
+        
+
+#     bench meal        
+
+#     orderID       101    102     103  104  105  106   107   108
+#     BenchNIM      4      4        4    5     5    6    4 
+#     mealID        1      2        3    1     2    2    
+
+
+#     meal_ingredient;
+#     combo  101  102  103
+#     meal       
